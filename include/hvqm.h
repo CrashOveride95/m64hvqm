0fa3d7c5fb844917ed99d37e4669eaf2846efab8
CrashOveride95 <crashoveride953@gmail.com>
2020-04-20 17:47:22 -0400

    Added HVQM includes

---
+++ include/hvqm.h
@@ -0,0 +1,13 @@
+#ifndef _HVQM_H_
+#define _HVQM_H_
+
+#ifndef _LANGUAGE_C
+#define _LANGUAGE_C
+#endif
+
+#include <HVQM/adpcmdec.h>
+#include <HVQM/hvqm2dec.h>
+#include <HVQM/HVQM2File.h>
+#include <HVQM/HVQmd.h>
+
+#endif
